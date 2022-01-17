1d3b8b0d0abf88ea1a40e12703771a525203a175
Anderson Felipe Pereira Vieira <anderson.vieira@santuario.local>
2022-01-17 10:28:30 -0300

    Header de produto

--- src/Product/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.KeyboardAvoidingView`
-    flex: 1;
-    background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
-`;
---
+++ src/screens/Product/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Platform, TouchableOpacity } from 'react-native';
+
+import { Container, Header, Title, DeleteLabel } from './styles';
+
+export function Product() {
+    return (
+        <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+            <Header>
+                <Title>Cadastrar</Title>
+                <TouchableOpacity>
+                    <DeleteLabel>Deletar</DeleteLabel>
+                </TouchableOpacity>
+            </Header>
+        </Container>
+    )
+}
