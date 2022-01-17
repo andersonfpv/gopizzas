3c353a70c2af16828cd30dbd201c90c7459ca3f5
Anderson Felipe Pereira Vieira <anderson.vieira@santuario.local>
2022-01-17 09:45:38 -0300

    Interface de produto

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "@hooks/auth";
 
 import theme from "./src/theme";
 
-import { SignIn } from "@screens/SignIn";
+import { Product } from "@screens/Product";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +25,7 @@ export default function App() {
       <StatusBar style="light" translucent backgroundColor="transparent" />
       
       <AuthProvider>
-        <SignIn />
+        <Product />
       </AuthProvider>
     </ThemeProvider>
   );
