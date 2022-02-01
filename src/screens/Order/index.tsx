beb2a6c4511112b6fe1f6392964f60d564674dc5
Anderson Felipe <anderson_fpv@yahoo.com.br>
2022-01-31 21:54:18 -0300

    Formulário de pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -5,29 +5,66 @@ import { PIZZA_TYPES } from "@utils/pizzaTypes";
 
 import { ButtonBack } from "@components/ButtonBack";
 import { RadioButton } from "@components/RadioButton";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
-import { Container, Header, Photo, Sizes } from "./styles";
+import { 
+    Container, 
+    ContentScroll, 
+    Header, 
+    Photo, 
+    Sizes, 
+    Form,
+    Title,
+    Label,
+    FormRow,
+    InputGroup,
+    Price
+} from "./styles";
 
 export function Order() {
     const [size, setSize] = useState('');
     return (
       <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-        <Header>
-          <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-        </Header>
+        <ContentScroll>
 
-        <Photo source={{ uri: "http://github.com/andersonfpv.png" }} />
+            <Header>
+            <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
+            </Header>
 
-        <Sizes>
-          {PIZZA_TYPES.map((item) => (
-            <RadioButton
-              key={item.id}
-              title={item.name}
-              onPress={() => setSize(item.id)}
-              selected={size === item.id}
-            />
-          ))}
-        </Sizes>
+            <Photo source={{ uri: "http://github.com/andersonfpv.png" }} />
+
+            <Form>
+                <Title>Nome da Pizza</Title>
+                <Label>Selecione um tamanho</Label>
+                <Sizes>
+                {PIZZA_TYPES.map((item) => (
+                    <RadioButton
+                    key={item.id}
+                    title={item.name}
+                    onPress={() => setSize(item.id)}
+                    selected={size === item.id}
+                    />
+                ))}
+                </Sizes>
+
+                <FormRow>
+                    <InputGroup>
+                        <Label>Número da mesa</Label>
+                        <Input keyboardType="numeric" />
+                    </InputGroup>
+                    
+                    <InputGroup>
+                        <Label>Quantidade</Label>
+                        <Input keyboardType="numeric" />
+                    </InputGroup>
+                </FormRow>
+                <Price>Valor de R$ 00,00</Price>
+                
+                <Button title="Confirmar pedido" />
+            </Form>
+        </ContentScroll>
+
       </Container>
     );
 }
